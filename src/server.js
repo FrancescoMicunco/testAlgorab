38bb62659d9824357112d4b53e5eef430f829d62
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2023-01-20 10:10:24 +0100

    creata una connessione al DB primario

--- src/server.js
+++ src/server.js
@@ -1,10 +1,27 @@
 import express from "express";
 import cors from "cors";
+import listEndpoints from "express-list-endpoints";
+import mongoose from "mongoose";
 
 const server = express();
 const port = process.env.PORT || 3000;
 
+// ========= middlewares ============
 server.use(cors());
 server.use(express.json());
 
-server.listen(port, () => console.log("server connected on port: ", port));
+// ========= Endpoints ==========
+
+// ========= Connections ========
+
+mongoose.connect(
+    process.env.MONGO_CONNECTION_STRING || "mongodb://localhost:27017"
+);
+
+mongoose.connection.on("connected", () =>
+    server.listen(port, () => console.log("server connected on port: ", port))
+);
+
+mongoose.connection.on("error", () => {
+    console.log("impossible to connect Database");
+});
