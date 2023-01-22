019e0aeea7622c0dbe50b1ca45a63b0b86ddcaec
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2023-01-22 10:34:15 +0100

    busmessanger_producer

--- src/apis/basicRequest/index.js
+++ src/apis/basicRequest/index.js
@@ -1,12 +1,16 @@
 import MessageModel from "./schema.js";
 import lodash from "lodash";
+import amqp from "amqplib/callback_api.js";
 
 // creaiamo la funzione utile a gestire le richieste dirette al DB principale.
 
 export const sendMessage = async(req, res, next) => {
     const message = req.body.message;
     const valuesArray = message.map((v) => v.value);
-    const totalValues = lodash.sum(valuesArray);
+    const sumValues = lodash.sum(valuesArray).toString();
+    if (sumValues) {
+        busController(sumValues);
+    }
 
     try {
         const newMessage = new MessageModel(req.body);
@@ -17,4 +21,23 @@ export const sendMessage = async(req, res, next) => {
     } catch (error) {
         next(error);
     }
+};
+
+const busController = (msg) => {
+    amqp.connect("amqp://localhost", function(error0, connection) {
+        if (error0) {
+            throw error0;
+        }
+        connection.createChannel(function(error1, channel) {
+            if (error1) {
+                throw error1;
+            }
+
+            const queue = "sum";
+
+            channel.assertQueue(queue, { durable: false });
+
+            channel.sendToQueue(queue, Buffer.from(msg));
+        });
+    });
 };
