994975eca22185cebf18093edae2933129f4a75d
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2023-01-22 09:19:45 +0100

    lodash per eseguire velocemente operazioni sull'array

--- src/apis/basicRequest/index.js
+++ src/apis/basicRequest/index.js
@@ -1,8 +1,13 @@
 import MessageModel from "./schema.js";
+import lodash from "lodash";
 
 // creaiamo la funzione utile a gestire le richieste dirette al DB principale.
 
 export const sendMessage = async(req, res, next) => {
+    const message = req.body.message;
+    const valuesArray = message.map((v) => v.value);
+    const totalValues = lodash.sum(valuesArray);
+
     try {
         const newMessage = new MessageModel(req.body);
 
